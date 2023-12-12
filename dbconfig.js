dfe15210b732266a92f0fd1623ed5361ea18666b
Pratik Dahal <prat@pop-os.localdomain>
2023-12-12 08:59:58 +0545

    rejectUnauthorized option set to false in ssl configuration

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,10 @@
 import pg from 'pg'
 const Pool = pg.Pool
 const pool = new Pool({
-  connectionString: 'postgres://prat:IlQFXEDdOi71G8rOjONrnoc0BBFJYUdd@dpg-clngcnhll56s73fhe6v0-a/eauction'
+  connectionString: 'postgres://prat:IlQFXEDdOi71G8rOjONrnoc0BBFJYUdd@dpg-clngcnhll56s73fhe6v0-a/eauction',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool
